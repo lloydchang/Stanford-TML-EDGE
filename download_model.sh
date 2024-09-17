352b18130ca043c32ca0f1456c7c9aa91bc8fa7c
lloydchang <lloydchang@gmail.com>
2024-09-17 15:19:22 -0700

    feat: use wget -c to continue downloading if interrupted

--- download_model.sh
+++ download_model.sh
@@ -1,1 +1,5 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1BAR712cVEqB8GR37fcEihRV_xOC-fZrZ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1BAR712cVEqB8GR37fcEihRV_xOC-fZrZ" -O checkpoint.pt && rm -rf /tmp/cookies.txt
+#!/bin/bash -x
+
+# use wget -c to continue downloading if interrupted
+
+wget -c --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget -c --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1BAR712cVEqB8GR37fcEihRV_xOC-fZrZ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1BAR712cVEqB8GR37fcEihRV_xOC-fZrZ" -O checkpoint.pt && rm -rf /tmp/cookies.txt
