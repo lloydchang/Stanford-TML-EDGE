a6224740d6794fb9b60f5c2ef70d72bff4216abb
lloydchang <lloydchang@gmail.com>
2024-09-17 23:20:09 -0700

    refactor: add try except

--- train.py
+++ train.py
@@ -1,11 +1,15 @@
 from args import parse_train_opt
 from EDGE import EDGE
-
+import traceback
 
 def train(opt):
     model = EDGE(opt.feature_type)
-    model.train_loop(opt)
-
+    try:
+        model.train_loop(opt)
+    except Exception as e:
+        print(f"An error occurred during training: {str(e)}")
+        print("Traceback:")
+        print(traceback.format_exc())
 
 if __name__ == "__main__":
     opt = parse_train_opt()
